9d0cc385250f01227b7cb7bf6b007fd632c3cc9a
daum <colin.daum@apirious.com>
2020-02-11 12:31:44 +0100

    Added isDisposed flag to ViewModel

--- lib/src/viewmodel.dart
+++ lib/src/viewmodel.dart
@@ -4,10 +4,12 @@ import 'package:flutter/widgets.dart';
 import 'package:viewmodel/src/ui_event.dart';
 
 abstract class ViewModel with ChangeNotifier {
+  bool _isDisposed = false;
   final _uiEvents = StreamController<UIEvent>.broadcast();
   Stream<UIEvent> get uiEvents => _uiEvents.stream;
 
   void postEvent(UIEvent event) {
+    if (_isDisposed) return;
     _uiEvents.add(event);
   }
 
@@ -18,6 +20,7 @@ abstract class ViewModel with ChangeNotifier {
   @override
   @mustCallSuper
   void notifyListeners() {
+    if (_isDisposed) return;
     try {
       super.notifyListeners();
     } catch (e, s) {
@@ -29,6 +32,8 @@ abstract class ViewModel with ChangeNotifier {
   @override
   @mustCallSuper
   void dispose() {
+    if (_isDisposed) return;
+    _isDisposed = true;
     _uiEvents.close();
     super.dispose();
   }
